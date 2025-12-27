a463c4edcb874d5f0466814dd0a3c5a9598276cb
berkbavas <berkbavas@gmail.com>
2025-12-27 14:32:43 +0300

    Add translations for project courses page.

---
+++ assets/js/project-courses.js
@@ -0,0 +1,17 @@
+import {TRANSLATIONS} from '../data/translations.js';
+
+// Add translations specific to Project Courses page
+
+TRANSLATIONS.en.headerTitle = "Project Courses";
+TRANSLATIONS.en.headerDescription = "Project-based courses offered by the Department of Mathematics, including senior projects and summer projects.";
+TRANSLATIONS.en.textViewCourseInformation = "View course information →";
+TRANSLATIONS.en.textViewCourseGuide = "Course guide →";
+TRANSLATIONS.en.textProjectDetails = "Project details →";
+TRANSLATIONS.en.textProjectArchive = "Project Archive";
+
+TRANSLATIONS.tr.headerTitle = "Proje Dersleri";
+TRANSLATIONS.tr.headerDescription = "Matematik Bölümü tarafından sunulan proje tabanlı dersler, mezuniyet projeleri ve yaz projeleri.";
+TRANSLATIONS.tr.textViewCourseInformation = "Ders bilgilerini görüntüle →";
+TRANSLATIONS.tr.textViewCourseGuide = "Ders kılavuzu →";
+TRANSLATIONS.tr.textProjectDetails = "Proje detayları →";
+TRANSLATIONS.tr.textProjectArchive = "Proje Arşivi";
